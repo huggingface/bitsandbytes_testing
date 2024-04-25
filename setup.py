375f2664b1241329f77ae39cfdda6b21681ce8e1
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2024-04-25 15:30:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=["torch", "numpy"],
     extras_require={
         "benchmark": ["pandas", "matplotlib"],
-        "test": ["scipy"],
+        "test": ["scipy", "pytest-reportlog", "pytest-cov", "parameterized", "datasets", "einops"],
     },
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
